4107af1e64f98b33b4bf5305fa0f5be22c13285c
RyanCMW <ryan.serrano@callmewine.com>
2023-03-31 16:20:06 +0200

    hotfix: bug fixing

--- middleware/category.js
+++ middleware/category.js
@@ -1,7 +1,7 @@
 export default async function ({ route, redirect }) {
-  if (route.path.split('-').at(-1).startsWith('B') && route.path.includes('cantina')) {
+  if (route.path.split('-').at(-1).startsWith('B') && route.path.includes('/cantina/')) {
     let handle = ''
-    handle = route.path.replace('cantina', 'winery')
+    handle = route.path.replace('/cantina/', '/winery/')
     handle = handle.replace('.htm', '')
 
     return redirect(handle)
