74471bf0d78c51e63ffabed6ca76f34fc750c2c2
RyanCMW <ryan.serrano@callmewine.com>
2023-05-07 02:32:40 +0200

    feat: replace slick for ssr carousel on home boxes

--- components/Home/HomeBoxes.vue
+++ components/Home/HomeBoxes.vue
@@ -1,68 +1,44 @@
 <script>
-import { computed, ref, useContext, useFetch } from '@nuxtjs/composition-api'
-import VueSlickCarousel from 'vue-slick-carousel'
+import { ref, useContext, useFetch } from '@nuxtjs/composition-api'
 import documents from '../../prismic-mapper'
 import { generateKey } from '@/utilities/strings'
 import { inRange } from '@/utilities/math'
-import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 
 export default {
-  components: { VueSlickCarousel },
   setup() {
-    const { $config, i18n, $prismic } = useContext()
+    const { $config, i18n, $prismic, $sentry } = useContext()
+    const carousel = ref(null)
 
     const boxes = ref([])
-    const carouselSettings = {
-      dots: true,
-      focusOnSelect: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      touchThreshold: 5,
-      responsive: [
-        {
-          breakpoint: 411,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: true,
-            arrows: false,
-          },
-        },
-        {
-          breakpoint: 767,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            dots: true,
-            arrows: false,
-          },
-        },
-        {
-          breakpoint: 1023,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            dots: false,
-            arrows: false,
-          },
-        },
-      ],
-    }
-
-    const c1 = ref(null)
-    const currentSlide = computed(() => c1.value && c1.value.$refs.innerSlider.currentSlide)
+    const responsive = [
+      {
+        minWidth: 0,
+        slidesPerPage: 1,
+      },
+      {
+        minWidth: 767,
+        slidesPerPage: 2,
+      },
+      {
+        minWidth: 1023,
+        slidesPerPage: 3,
+      },
+    ]
 
     const { fetch } = useFetch(async () => {
-      const { data } = await $prismic.api.getSingle(
+      await $prismic.api.getSingle(
         documents[$config.STORE].homeBoxes,
         { lang: i18n.localeProperties.iso.toLowerCase() },
       )
-      boxes.value = data.box
+        .then(({ data }) => {
+          boxes.value = data.box
+        })
+        .catch((err) => {
+          $sentry.captureException(new Error(`Catch getting homeBoxes from prismic: ${err}`))
+        })
     })
 
-    return { c1, currentSlide, carouselSettings, fetch, boxes }
+    return { carousel, responsive, fetch, boxes }
   },
 
   methods: { inRange, generateKey },
@@ -70,8 +46,11 @@ export default {
 </script>
 
 <template>
-  <div v-if="!!boxes.length" class="cmw-max-w-screen-xl cmw-mx-auto cmw-py-4 cmw-px-4 cmw-mt-4">
-    <VueSlickCarousel ref="c1" v-bind="carouselSettings" dots-class="c-carouselDots">
+  <div v-if="!!boxes.length" class="cmw-max-w-screen-xl cmw-mx-auto cmw-py-8 cmw-px-4 cmw-mt-4">
+    <SsrCarousel
+      ref="carousel" :key="boxes.length" :responsive="responsive" :show-arrows="false"
+      :show-dots="false" class="cmw-relative"
+    >
       <div v-for="({ title, description, image }) in boxes" :key="generateKey(title)" class="cmw-h-full cmw-pt-8">
         <div class="cmw-relative cmw-rounded cmw-border cmw-border-gray-light cmw-px-4 cmw-text-center cmw-h-full cmw-pt-12 cmw-pb-2">
           <div class="cmw-absolute cmw-flex cmw-bg-white cmw-transform cmw-left-1/2 cmw-top-0 cmw-translate-x-[-50%] cmw-translate-y-[-50%] cmw-px-2 cmw-w-[100px] cmw-h-[100px]">
@@ -87,42 +66,6 @@ export default {
           <p v-text="description" />
         </div>
       </div>
-      <template #customPaging="page">
-        <button
-          :data-test="page"
-          class="c-carouselDots__dot"
-          :class="{ '-sm': !inRange((page - currentSlide), -2, 2) }"
-        />
-      </template>
-    </VueSlickCarousel>
+    </SsrCarousel>
   </div>
 </template>
-
-<style lang="css" scoped>
-::v-deep(.slick-track) {
-  display: flex !important;
-}
-
-::v-deep(.slick-slide) > div {
-  height: 100%;
-}
-
-::v-deep(.slick-slide) {
-  padding-left: 8px;
-  padding-right: 8px;
-  height: inherit !important;
-}
-
-::v-deep(.c-carouselDots li) {
-  display: flex !important;
-}
-
-::v-deep(.slick-active .c-carouselDots__dot) {
-  transform: scale(1);
-  background-color: theme('colors.primary.DEFAULT');
-}
-
-::v-deep(li:not(.slick-active) .c-carouselDots__dot.-sm) {
-  transform: scale(0.3);
-}
-</style>
