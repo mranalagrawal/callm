ea29df457c8ceb40ac60be288d51167db2c82633
RyanCMW <ryan.serrano@callmewine.com>
2023-05-16 07:46:53 +0200

    feat: implement link on collections

---
+++ types/metafield.ts
@@ -0,0 +1,12 @@
+export interface Metafield {
+  // createdAt:DateTime!
+  // description:String
+  // id:ID!
+  // key:String!
+  // namespace:String!
+  // parentResource:String # Todo: MetafieldParentResource!
+  // reference:String # Todo: MetafieldReference
+  // type:String!
+  // updatedAt:DateTime!
+  value: string
+}
