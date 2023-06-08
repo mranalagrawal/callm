88a2545f1417db8462a774ece00cb38be2a0516d
RyanCMW <ryan.serrano@callmewine.com>
2023-06-08 18:35:37 +0200

    fix: test cache issue

---
+++ pages/fix-me.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  computed: {
+    userCart() {
+      return this.$store.state.userCart.userCart
+    },
+  },
+})
+</script>
+
+<template>
+  <pre>{{ userCart }}</pre>
+</template>
