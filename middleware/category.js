dd18ba100c32e8da1098149b2c4f5614fab3bcdf
RyanCMW <ryan.serrano@callmewine.com>
2023-06-20 16:09:53 +0200

    feat: implement locale winery pages

--- middleware/category.js
+++
@@ -1,9 +0,0 @@
-export default async function ({ route, redirect }) {
-  if (route.path.split('-') && route.path.split('-').at(-1).startsWith('B') && route.path.includes('/cantina/')) {
-    let handle = ''
-    handle = route.path.replace('/cantina/', '/winery/')
-    handle = handle.replace('.htm', '')
-
-    return redirect(handle)
-  }
-}
