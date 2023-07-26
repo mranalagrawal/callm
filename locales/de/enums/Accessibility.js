494086957b2eefc3e093248abc26fd3b98e54f03
RyanCMW <ryan.serrano@callmewine.com>
2023-07-26 16:49:37 +0200

    feat: add missing labels

--- locales/de/enums/Accessibility.js
+++ locales/de/enums/Accessibility.js
@@ -5,6 +5,18 @@ export default {
       prev: 'Nächste',
     },
     role: {
+      MENU: {
+        OPEN: 'Menü öffnen',
+        CLOSE: 'Menü schließen',
+      },
+      MENU_LOGIN: {
+        OPEN: 'Anmeldemenü öffnen',
+        CLOSE: 'Anmeldemenü schließen',
+      },
+      MENU_NAVIGATION_USER: {
+        OPEN: 'Öffnen Sie das Benutzernavigationsmenü',
+        CLOSE: 'Benutzernavigationsmenü schließen',
+      },
       TRIGGER_SEARCH: 'Suche auslösen',
       ADD_TO_CART: 'In der CartAdd des Warenkorb',
       MODAL_OPEN: 'Modales Fenster öffnen',
@@ -14,7 +26,8 @@ export default {
       REMOVE_FROM_WISHLIST: 'Von Wunschliste entfernen',
     },
     labels: {
-      GO_TO_HOME_PAGE: 'Zur Startseite navigieren',
+      GO_TO_CART_PAGE: 'Navigate to the cart page',
+      GO_TO_HOME_PAGE: 'Navigieren Sie zur Warenkorbseite',
       GO_TO_PAGE: 'Gehe zu {url}',
       views: {
         GRID: 'Rasteransicht',
--- locales/fr/enums/Accessibility.js
+++ locales/fr/enums/Accessibility.js
@@ -5,6 +5,18 @@ export default {
       prev: 'Suivant',
     },
     role: {
+      MENU: {
+        OPEN: 'Ouvrir le menu',
+        CLOSE: 'Fermer le menu',
+      },
+      MENU_LOGIN: {
+        OPEN: 'Ouvrir le menu de connexion',
+        CLOSE: 'Fermer le menu de connexion',
+      },
+      MENU_NAVIGATION_USER: {
+        OPEN: 'Ouvrir le menu de navigation utilisateur',
+        CLOSE: 'Fermer le menu de navigation utilisateur',
+      },
       TRIGGER_SEARCH: 'Recherche de déclencheur',
       ADD_TO_CART: 'Ajouter au panier',
       MODAL_OPEN: 'Ouvrir la fenêtre modale',
@@ -14,6 +26,7 @@ export default {
       REMOVE_FROM_WISHLIST: 'Supprimer de la liste de souhaits',
     },
     labels: {
+      GO_TO_CART_PAGE: 'Accédez à la page du panier',
       GO_TO_HOME_PAGE: 'Naviguer vers la page d\'accueil',
       GO_TO_PAGE: 'Aller à {url}',
       views: {
