2878b814abb2ae98849cb2773852de32beb4a8ec
RyanCMW <ryansegus@gmail.com>
2023-10-16 13:47:49 +0200

    feat: test round border

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -88,7 +88,7 @@ export default defineComponent({
       v-model="search"
       type="search"
       class="
-               c-searchInput -hasIcon px-4 text-gray-dark py-3 w-full bg-transparent border border-gray-light rounded
+               c-searchInput -hasIcon px-4 text-gray-dark py-3 w-full bg-transparent border border-gray-light rounded-[12px]
                hover:(border-gray)
                focus:(outline-none border-gray-dark)"
       :placeholder="$t('navbar.search')"
