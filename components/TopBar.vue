5e6ad4c920edd7b86a7f2d648631d9de81b8e04a
RyanCMW <ryansegus@gmail.com>
2023-10-16 10:36:45 +0200

    feat: cmw 790

--- components/TopBar.vue
+++ components/TopBar.vue
@@ -4,17 +4,19 @@ import { defineComponent, ref, useFetch } from '@nuxtjs/composition-api'
 export default defineComponent({
   setup() {
     const headline = ref('')
+    const headlineSm = ref('')
 
     useFetch(async ({ $cmwRepo }) => {
       await $cmwRepo.prismic.getSingle('topbar')
         .then((data) => {
           if (!data.text?.length) { return }
 
-          headline.value = data.text[0].text
+          headline.value = data.text ?? undefined
+          headlineSm.value = data.text_sm ?? undefined
         })
     })
 
-    return { headline }
+    return { headline, headlineSm }
   },
 })
 </script>
@@ -22,11 +24,9 @@ export default defineComponent({
 <template>
   <div
     class="print:hidden w-screen px-4 h-$cmw-top-banner-height text-center
-     bg-secondary fixed top-0 left-0 z-1050 text-white uppercase py-1 line-clamp-2 leading-4
-     overline-1 md:overline-2"
+     bg-secondary fixed top-0 left-0 z-1050 text-white uppercase py-1 line-clamp-2 leading-4 overline-2"
   >
-    <div
-      v-html="headline"
-    />
+    <PrismicText v-if="headlineSm" class="md:hidden" :field="headlineSm" />
+    <PrismicText v-if="headline" class="<md:hidden" :field="headline" />
   </div>
 </template>
