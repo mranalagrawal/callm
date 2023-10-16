2ba3b3d184dd8e4fd3e55a5c1a4fb351aa32b8b7
RyanCMW <ryansegus@gmail.com>
2023-10-16 12:58:09 +0200

    feat: lazy loading

--- components/Base/LoadingImage.vue
+++ components/Base/LoadingImage.vue
@@ -81,8 +81,8 @@ export default {
   <component :is="wrapper" ref="el" class="w-full">
     <img
       ref="imgEl"
-      class="filter transition-lazy-image duration-100 text-xxs"
-      :class="[imageIsLoaded ? 'blur-0 opacity-100' : 'opacity-50 blur-1', imgClasses]"
+      class="filter text-xxs"
+      :class="[imageIsLoaded ? 'blur-0 opacity-100' : 'opacity-0 blur-0', imgClasses]"
       :data-src="source.url"
       :data-thumbnail="thumbnail.url"
       :alt="thumbnail.altText"
