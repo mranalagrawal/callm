4bc7d04d7e9fe430ad431f52112ffa3304143ec6
RyanCMW <ryansegus@gmail.com>
2024-02-06 12:33:03 +0100

    feat: products-availability-alerts

--- store/product-availability.ts
+++ store/product-availability.ts
@@ -1,9 +1,12 @@
 import { defineStore } from 'pinia'
 
+interface IState {
+  productId: string | number | null
+}
+
 export const useProductAvailability = defineStore({
   id: 'productAvailability',
-  state: () => ({
+  state: () => <IState> ({
     productId: null,
   }),
-
 })
